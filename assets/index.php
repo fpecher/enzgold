723e535d170a903ad636735bc86a3058867dde8a
LukasBirringer <lukasbirringer@Lulumbp.fritz.box>
2015-08-13 17:00:19 +0200

    Home Portfolio Hoher der kein Hoher ist

---
+++ assets/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="css/main.css">
+	<title>Enzgold Assets</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div class="headline-wrapper">
+		<h1>Portfolio</h1>
+		<div class="hline"></div>
+		<p class="sub-headline">eine Auswahl unserer Projekte</p>
+	</div>
+	<div class="landingpage-portfolio">
+		<div class="preview-portfolio pv1">
+			<div class="table">
+				<div class="table-cell">
+					<div class="pv-hover">
+						<div class="square">
+							<img class="hover-icon link" src="img/hover-icon.png" alt="">
+							<p class="hover-firma" >TSV Mühlhausen</p>
+							<p class="hover-branche" >Vereinsseite</p>
+							<img class="hover-icon hover-drop" src="img/hover-drop.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="preview-portfolio pv2">
+			<div class="table">
+				<div class="table-cell">
+					<div class="pv-hover">
+						<div class="square">
+							<img class="hover-icon link" src="img/hover-icon.png" alt="">
+							<p class="hover-firma" >TSV Mühlhausen</p>
+							<p class="hover-branche" >Vereinsseite</p>
+							<img class="hover-icon hover-drop" src="img/hover-drop.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="preview-portfolio pv3"></div>
+		<div class="preview-portfolio pv4"></div>
+	</div>
+</body>
+</html>
