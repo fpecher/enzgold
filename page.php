d2f101252ac6abcedc04da39be7fedbbd193e186
Fabian <pecherfabian@gmail.com>
2015-09-03 21:34:15 +0200

    content link styles

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 		<div class="row">
 			<div class="col-md-12">
 				<?php while ( have_posts() ) : the_post(); ?>
-					<h1><?php the_title(); ?></h1>
+					<h1 class="mb80"><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 				<?php endwhile; ?>
 			</div>
