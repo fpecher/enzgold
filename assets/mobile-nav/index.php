46ae3ee5b04f335a386afb8f0acd089f66e2aa96
LukasBirringer <lukasbirringer@Lulumbp.fritz.box>
2015-09-07 20:19:05 +0200

    Mobile-nav

---
+++ assets/mobile-nav/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<title>Document</title>
+</head>
+<body>
+	<div class="container">
+			<div class="menu-wrap">
+				<nav class="menu-top">
+					
+				</nav>
+				<nav class="menu-side">
+					<a href="#">Home</a>
+					<a href="#">Portfolio</a>
+					<a href="#">enzgold</a>
+					<a href="#">Kontakt</a>
+				</nav>
+			</div>
+			<button class="menu-button" id="open-button">Open Menu</button>
+			<div class="content-wrap">
+				<div class="content">
+					<!-- Content Here -->
+				</div>
+			</div><!-- /content-wrap -->
+		</div><!-- /container -->
+		<script src="js/classie.js"></script>
+		<script src="js/main.js"></script>
+</body>
+</html>
